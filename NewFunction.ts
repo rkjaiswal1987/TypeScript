1302fc4107eb58f233b589cf72b6fb3934b89857
rkjaiswal1987 <140277886+rkjaiswal1987@users.noreply.github.com>
2023-07-25 00:13:16 +0530

    two new example

--- NewFunction.ts
+++ NewFunction.ts
@@ -20,3 +20,20 @@ const Addition=(x:number,y:number):number=>{
 }
 
 console.log(Addition(2,3));
+
+/////Complex example Anonymous and arrow functio--
+
+let Data =[10,20,30,40,50];
+
+//Anonymous
+
+let NewData= Data.map(function(item){
+    return item*2;
+    });
+    console.warn(NewData);
+
+//arrow function
+
+let NewDataArrow = Data.map((item)=>item*2);
+
+console.warn(NewDataArrow);
--- Newfunction.js
+++ Newfunction.js
@@ -13,3 +13,13 @@ var Addition = function (x, y) {
     return x * y;
 };
 console.log(Addition(2, 3));
+/////Complex example Anonymous and arrow functio--
+var Data = [10, 20, 30, 40, 50];
+//Anonymous
+var NewData = Data.map(function (item) {
+    return item * 2;
+});
+console.warn(NewData);
+//arrow function
+var NewDataArrow = Data.map(function (item) { return item * 2; });
+console.warn(NewDataArrow);
